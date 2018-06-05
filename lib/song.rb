aaa743b553923adfe2201a1f25c06840c145569b
Ariel Zhen <zhenpingping@live.com>
2018-06-05 04:38:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,9 +7,14 @@ class Song
     @name = name
   end
 
+  def artist_name=(name) 
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end 
+  
   def self.new_by_filename(filename)
     song = Song.new  
-    song.artist = filename.split(" ")[0]
+    song.artist = filename.chomp(".mp3").split(" ")[0]
     song.name = filename.split(" ")[1]
     song
   end
