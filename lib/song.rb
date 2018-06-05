dd808c33a67475388fdc9500ad4ce21867892485
Ariel Zhen <zhenpingping@live.com>
2018-06-05 04:14:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,14 @@ require 'pry'
 class Song
 
   attr_accessor :name, :artist 
+  
   def initialize(name)
     @name = name
   end
 
-  def name=(name)
-    @name = name
-  end
+  # def name=(name)
+  #   @name = name
+  # end
 
   def self.new_by_filename(filename)
     song = Song.new
