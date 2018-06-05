b28bd9a4cdd06ade301f824bf5883abcddbbe041
Ariel Zhen <zhenpingping@live.com>
2018-06-05 06:37:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -33,8 +33,8 @@ class Artist
     end
 
     def self.find_by_name(artist_name)
-       Artist.all.detect {|artist| artist.name == artist_name}
-     
+       Artist.all.select {|artist| artist.name == artist_name}
+
       # binding.pry
       #  @@all.each do |artist|
       #    if @@all.include? artist_name
