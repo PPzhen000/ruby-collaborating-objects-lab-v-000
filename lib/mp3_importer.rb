815a21a9234192d533f5e4eac5577930160e3590
Ariel Zhen <zhenpingping@live.com>
2018-06-05 03:52:05 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,11 +5,14 @@ class MP3Importer
   attr_accessor :path
 
   def initialize(file_path)
-    @path = file_path #@path = "./spec/fixtures/mp3s" 
+    @path = file_path #@path = "./spec/fixtures/mp3s"
   end
 
   def files
-
+    @files = []
+    Dir.glob("#{path}/*.mp3").collect do |f|
+      @files << f
+    end 
   end
 
   def import
