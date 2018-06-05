a454d5904cc06c8650af8dbd89891a7e64d08c13
Ariel Zhen <zhenpingping@live.com>
2018-06-05 04:39:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
-  
+
   def self.new_by_filename(filename)
     song = Song.new(filename.chomp(".mp3").split(" - ")[1])
     song.artist = filename.chomp(".mp3").split(" - ")[0]
