b54def6494c4a72c1e6c1765e2d905824b06caca
Ariel Zhen <zhenpingping@live.com>
2018-06-05 04:45:31 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,43 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :song 
 
   @@all = []
 
   def initialize(artist_name)
     @name = artist_name
+    @songs = [] 
   end
 
-  def add_song
-    song = Song.new(name)
-    # @@all << song if song.artist.name
+  def add_song(song)
+    self.songs << song 
   end
 
   def songs
-
+    song = Song.new 
   end
 
   def save
-    artist = Artist.new
     @@all << self 
   end
 
   def self.find_or_create_by_name
-
+    if self.find(name)
+      self.find(name)
+    else 
+      self.create(name)
+    end 
   end
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name} 
+  end 
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save 
+    artist
+  end 
 
   def print_songs
 
