37de02f97d540c752b6e972b31750ac161ae85a1
Ariel Zhen <zhenpingping@live.com>
2018-06-05 03:50:16 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class MP3Importer
+
+  attr_accessor :path
+
+  def initialize(file_path)
+    @path = file_path #@path = "./spec/fixtures/mp3s" 
+  end
+
+  def files
+
+  end
+
+  def import
+
+  end
+
+end
