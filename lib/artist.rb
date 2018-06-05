918bb18512cacc460490d560079e6f68a6f2b846
Ariel Zhen <zhenpingping@live.com>
2018-06-05 06:11:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,7 +32,7 @@ class Artist
     end
 
     def self.find_by_name(artist_name)
-      @@all.select {|artist| artist.name == artist_name}
+      @@all.detect {|artist| artist.name == artist_name}
     end
 
     def self.find_or_create_by_name(artist_name)
