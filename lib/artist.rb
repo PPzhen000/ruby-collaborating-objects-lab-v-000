beb3cae291075033cb7740ba65769f2e01e0b062
Ariel Zhen <zhenpingping@live.com>
2018-06-05 04:18:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,26 @@
 class Artist
 
+  attr_accessor :name 
+
+  def initialize(artist_name)
+    @name = artist_name
+  end 
+
+  def songs 
+
+  end 
+
+  def save
+
+  end 
+
+  def self.find_or_create_by_name
+
+  end 
+
+  def print_songs
+
+  end 
+  
 
 end 
