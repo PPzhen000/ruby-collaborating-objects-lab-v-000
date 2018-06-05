3b9f2d70c40574fee62d5918b3bad92ed19df229
Ariel Zhen <zhenpingping@live.com>
2018-06-05 07:12:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,9 @@ class Song
 
   def self.new_by_filename(filename)
     filename = filename.chomp(".mp3").split(" - ")
-    song = Song.new(filename[1])
-    song.artist = filename[0]
-    song.artist_name=(filename[1])
+    song = Song.new(filename[0])
+    song.artist = filename[1]
+    song.artist_name=(filename[0])
     #binding.pry
     song
   end
