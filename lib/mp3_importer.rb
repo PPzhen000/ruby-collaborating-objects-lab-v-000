f49c1cabcbff58f0a08077dff92dd4a9c5abb75d
Ariel Zhen <zhenpingping@live.com>
2018-06-05 04:37:00 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -16,8 +16,8 @@ class MP3Importer
     @files.uniq
   end
 
-  def import(list_of_files)
-    list_of_files.each { |filename| Song.new_by_filename(filename)}  
+  def import
+    self.files.each { |filename| Song.new_by_filename(filename)}  
   end
 
 end
